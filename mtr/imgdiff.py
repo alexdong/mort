bdd412761395d44f98d26be7cc3c3ab26d386a75
Alex Dong <me@alexdong.com>
2017-10-12 07:11:23 +1300

    some clean up to bring pylint back to 10.0/10.0

--- mtr/imgdiff.py
+++ mtr/imgdiff.py
@@ -1,9 +1,9 @@
 import cv2
 import numpy
-from skimage.measure import structural_similarity as ssim
 from skimage.measure import compare_ssim
+from skimage.measure import structural_similarity as ssim
 
-# from .local_conf import IMAGE_DIFF_BOX_COLOR, IMAGE_DIFF_BOX_STROKE_WIDTH
+from .local_conf import IMAGE_DIFF_BOX_COLOR, IMAGE_DIFF_BOX_STROKE_WIDTH
 
 
 def load_image(img_file: str) -> numpy.ndarray:
@@ -41,14 +41,12 @@ def generate_annotated_diff_image(image_file: str, reference_file: str, as_file:
     :param as_file: the full path where the diff image will be saved.
     :return: the number of differences found
     """
-    # Use `compare_ssim(..., full=True)` to get the difference image.
-    # Please no
     reference = load_image(reference_file)
     image = load_image(image_file)
     (_, diff_image) = compare_ssim(image, reference, full=True)  # full will return the `diff_image`
 
     # First, let's convert the `diff_image` from float data between [0, 1] into unsigned int 8
-    diff_image =  (diff_image * 255).astype('uint8')
+    diff_image = (diff_image * 255).astype('uint8')
 
     # Threshold the difference using and find the regions where the two images differ.
     threshold_image = cv2.threshold(diff_image, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
@@ -60,12 +58,10 @@ def generate_annotated_diff_image(image_file: str, reference_file: str, as_file:
     # Annotate each regions by drawing a rectangle on the image
     color_image = cv2.imread(image_file)
     for (x, y, w, h) in region_boxes:
-        cv2.rectangle(color_image, (x, y), (x + w, y + h), (0, 0, 255),
-                      2)  # IMAGE_DIFF_BOX_COLOR, IMAGE_DIFF_BOX_STROKE_WIDTH)
+        cv2.rectangle(color_image, (x, y), (x + w, y + h), IMAGE_DIFF_BOX_COLOR, IMAGE_DIFF_BOX_STROKE_WIDTH)
     cv2.imwrite(as_file, color_image)
     return len(region_boxes)
 
-
 # if __name__ == "__main__":
-    # print(get_similarity_index('/tmp/edited.jpg', '/tmp/origin.jpg'))
-    # print(generate_annotated_diff_image('/tmp/edited.jpg', '/tmp/origin.jpg', '/tmp/output.jpg'))
+# print(get_similarity_index('/tmp/edited.jpg', '/tmp/origin.jpg'))
+# print(generate_annotated_diff_image('/tmp/edited.jpg', '/tmp/origin.jpg', '/tmp/output.jpg'))
