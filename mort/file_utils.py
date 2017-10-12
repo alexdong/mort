97e7252caee0ae9c642bedcbcb40a5446697fef8
Alex Dong <me@alexdong.com>
2017-10-13 07:06:27 +1300

    first version of the cli structure

--- mort/file_utils.py
+++ mort/file_utils.py
@@ -1,4 +1,11 @@
 import os
+import subprocess
+
+from mort.local_conf import SOURCE_CODE_PATH
+
+
+def get_git_hash(path: str = SOURCE_CODE_PATH) -> str:
+    return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'], cwd=path).strip().decode('utf-8')
 
 
 def create_dir_if_not_exists(path: str) -> bool:
--- mort/main.py
+++ mort/main.py
@@ -1,25 +1,38 @@
+import json
 import click
 
 from mort.driver import download_latest_target_list
-from mort.local_conf import TARGETS
-from mort.matcher import target_matches
+from mort.local_conf import TARGETS, PATHS, TARGET_LIST_FILE_PATH
+from mort.file_utils import get_git_hash
+from mort import controller
 
 
 @click.group()
-@click.option('--target_filter')
-@click.option('--url_filter')
+@click.option('--targets', default=json.dumps(TARGETS))
+@click.option('--paths', default=json.dumps(PATHS))
+@click.option('--git-hash', default=get_git_hash())
 @click.pass_context
-def cli(ctx, target_filter, url_filter):
-    ctx.obj = {"target_filter": target_filter, "urls": url_filter}
+def cli(ctx, targets, paths, git_hash):
+    ctx.obj = {"targets": json.loads(targets), "paths": json.loads(paths), "git_hash": git_hash}
 
 
 @cli.command()
 @click.pass_obj
-def show(ctx_obj):
-    print([target for target in TARGETS if target_matches(target, ctx_obj['target_filter'])])
+def capture(ctx):
+    controller.capture(ctx.obj['paths'], ctx.obj['urls'], ctx.obj['git_hash'])
 
 
 @cli.command()
-def update():
+@click.pass_obj
+@click.argument('ref-git-hash')
+def compare(ctx, ref_git_hash):
+    controller.compare(ctx.obj['paths'], ctx.obj['urls'], ctx.obj['git_hash'], ref_git_hash)
+
+
+@cli.command()
+@click.option('--save-as', default=TARGET_LIST_FILE_PATH)
+def update(save_as):
     print("Downloading the latest device and OS support list from BrowserStack ...")
-    download_latest_target_list("./targets.json")
+    download_latest_target_list(save_as)
+
+
